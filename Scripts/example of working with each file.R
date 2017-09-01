4cf3303864ab97e3a2eb916607f55b7889b2db06
rosseji <rosseireland@gmail.com>
2017-09-01 12:46:29 +1000

    what do you think

--- Scripts/example of working with each file.R
+++ Scripts/example of working with each file.R
@@ -1,6 +1,3 @@
-
-# sample run script
-
 files <- list.files("examples") %>%
   map( ~ list(
     name = str_replace(.x, ".xls", ""),
@@ -8,40 +5,29 @@ files <- list.files("examples") %>%
   )) %>%
   set_names(map_chr(., "name"))
 
+# do to each!
+
+input_ls <- files[[1]]
 
 
+var_meta <- input_ls$file_path %>%
+  read_excel(sheet = 1) %>%
+  meta_clean()
 
-ls <- files %>%
-  map( ~ glue("examples/{.x}")) %>%
-  map( ~ list(
-    meta = process_meta(.x),
-    data = process_data(.x)
-  ))
+working_tbl <- input_ls$file_path %>%
+  read_excel(sheet = 2) %>%
+  remove_meta()
 
+original_nms <- colnames(df_data)
+new_nms <- clean_names_full(original_nms)
+var_meta <- var_meta %>%
+  mutate(Data_Item_Description = new_nms[-1])
 
+new_nms[1] <- "Date"
+colnames(working_tbl) <- new_nms
 
-
-
-
-
-
-process_meta <- function(file_path) {
-  df <- file_path %>%
-    read_excel(sheet = 1) %>%
-    meta_clean()
-}
+working_tbl <- working_tbl %>%
+  mutate(Date = as.POSIXct(as.numeric(Date) * (60*60*24), origin="1899-12-30", tz="GMT"))
 
 
-process_data <- function(file_path) {
-  df <- file_path %>%
-    read_excel(sheet = 2) %>%
-    remove_meta()
-
-  original_nms <- colnames(df)
-  new_nms <- clean_names_full(original_nms)
-
-  list(data = df, original_nms = original_nms, new_nms = new_nms)
-}
-
-
-
+# get names
