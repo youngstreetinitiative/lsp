35a9d44af5845f7ae675109390abb183ac76d4e1
Jackosan6 <jackson.whitehair@gmail.com>
2017-09-15 16:32:20 +1000

    Additions to bundle sheets function to draw from dropbox folder

--- R/bundle sheets.R
+++ R/bundle sheets.R
@@ -1,7 +1,30 @@
 #' @export
 
-bundle_sheets <- function() {
+bundle_sheets <- function(DropboxDataFolder=0) {
 
+
+  if(DropboxDataFolder!=0){
+    paths::DropboxDirFN(DropboxOrigin = DropboxDataFolder)
+    print(DropboxDir)
+    files <- list.files(DropboxDir,pattern=".xlsx") %>%
+      map( ~ list(
+        name = str_replace(.x, ".xls", ""),
+        file_path =  glue("{DropboxDir}/{.x}")
+      )) %>%
+      set_names(map_chr(., "name"))
+
+    master_ls <- files %>%
+      map( ~ process_sheet(.x))
+
+    nms <- master_ls %>% map_chr("full_name")
+
+    master_ls <- master_ls %>%
+      set_names(nms)
+
+    rdspath <- glue("{DropboxDir}/master_ls.rds")
+    write_rds(master_ls, rdspath)
+    master_list <<- read_rds(rdspath)
+  }else{
   files <- list.files("data-raw") %>%
     map( ~ list(
       name = str_replace(.x, ".xls", ""),
@@ -18,5 +41,6 @@ bundle_sheets <- function() {
     set_names(nms)
 
   write_rds(master_ls, "data/master_ls.rds")
-
+  master_list <<- read_rds("data/master_ls.rds")
+}
 }
